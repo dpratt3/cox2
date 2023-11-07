7f4b6c866ed4014b4209719c138e74e09548ca90
David Pratt <david@midnightmechanism.com>
2023-11-07 00:11:49 -0600

    Change title to cyclooxygenase two classifier

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
       <div className="App">
-        <h1 style={{ color: 'white', fontSize: '36px'}}>Cox-2 Classifier</h1>
+        <h1 style={{ color: 'white', fontSize: '36px'}}>Cycloxygenase Two Classifier</h1>
         <div className="translucent">
           <StylizedHistogram />
           <Slider />
